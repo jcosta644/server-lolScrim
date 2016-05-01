caf60774687a0b8bb74b8b08a4ab7bfa581064d5
Pedro Soares <pedrosoares1542@gmail.com>
2016-05-01 14:31:12 -0300

    Adicionando a API de reducao do link ao front.js

--- requests/API.js
+++ requests/API.js
@@ -5,7 +5,12 @@ require("dotenv").load();
 const API = {
 	key: "?api_key=" + process.env.API_KEY,
 	base_URL: "https://br.api.pvp.net/api/lol/",
-	getSummonerByName: "/v1.4/summoner/by-name/"
+	getSummonerByName: "/v1.4/summoner/by-name/",
+	getSummonerById: "/v1.4/summoner/",
+	getLeagueBySummonerId: "/v2.5/league/by-summoner/",
+	getMatchListBySummonerId: "/v2.2/matchlist/by-summoner/",
+	getChampionStatsBySummonerId: "/v1.3/stats/by-summoner/",
+	filterRankedSeasontoMatchList: "?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016&api_key=" + process.env.API_KEY
 }
 
 module.exports = API;
--- requests/front.js
+++ requests/front.js
@@ -3,30 +3,33 @@
 require("dotenv").load();
 
 const request = require("request");
-
-const key = process.env.API_KEY;
+const API = require("./API");
 
 module.exports = {
     getSummonerByName(req, res){
 		let name = req.body.name;
 		let region = req.body.region;
 
-		request('https://br.api.pvp.net/api/lol/'+ region +'/v1.4/summoner/by-name/'+ name + '?api_key='+ key, function (error, response, body) {
-  			if (!error && response.statusCode == 200) {
-    			res.json(JSON.parse(body));
-  			}
-		});
+		request(
+			API.base_URL + region + API.getSummonerByName + name + API.key,
+				function (error, response, body) {
+		  			if (!error && response.statusCode == 200) {
+		    			res.json(JSON.parse(body));
+		  			}
+			});
 	},
 
 	getRankedLeague(req, res){
 		let id = req.body.id;
 		let region = req.body.region;
 
-		request('https://br.api.pvp.net/api/lol/'+ region +'/v2.5/league/by-summoner/'+ id + '/entry?api_key='+ key, function (error, response, body) {
-			if(!error && response.statusCode == 200) {
-				res.json(JSON.parse(body));
-			}
-		});
+		request(
+			API.base_URL + region + API.getLeagueBySummonerId + id + '/entry' + API.key,
+				function (error, response, body) {
+					if(!error && response.statusCode == 200) {
+						res.json(JSON.parse(body));
+					}
+			});
 	},
 
 	getChampionStatsBySummonerId(req, res){
@@ -35,40 +38,44 @@ module.exports = {
 		let lane = req.body.lane; //LANES: DUO_SUPPORT, DUO_CARRY, MID ,JUNGLE, TOP
 		let matchListLane = [],exist;
 
-		request('https://br.api.pvp.net/api/lol/'+ region +'/v2.2/matchlist/by-summoner/'+ id +'?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016&api_key='+ key, function(error, response, body){
-			if(!error && response.statusCode == 200) {
-				let match = JSON.parse(body);
-				match.matches.forEach(function(item, index){
-					if(item.lane == lane || item.role == lane){
-						matchListLane.push(item);
-					}
-				});
-				request('https://br.api.pvp.net/api/lol/'+ region +'/v1.3/stats/by-summoner/'+ id + '/ranked?api_key='+ key, function (error, response, body) {
+		request(
+			API.base_URL + region + API.getMatchListBySummonerId + id + API.filterRankedSeasontoMatchList,
+				function(error, response, body){
 					if(!error && response.statusCode == 200) {
-						let stats = JSON.parse(body), matchList = [];
-							matchListLane.forEach(function(item, index){
-								stats.champions.forEach(function(element, index){
-									if(item.champion == element.id && element.id != 0){
-										exist = false;
-										matchList.forEach(function(match, index){
-											if(element.id == match.id){
-												exist = true;
-											}
-										});
-										if(!exist){
-											matchList.push(element);
-										}
-									}
-								});
-							});
-						matchList.sort(function(a,b) {
-						    return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
+						let match = JSON.parse(body);
+						match.matches.forEach(function(item, index){
+							if(item.lane == lane || item.role == lane){
+								matchListLane.push(item);
+							}
 						});
+						request(
+							API.base_URL + region + API.getChampionStatsBySummonerId + id + '/ranked' + API.key,
+								function (error, response, body) {
+									if(!error && response.statusCode == 200) {
+										let stats = JSON.parse(body), matchList = [];
+											matchListLane.forEach(function(item, index){
+												stats.champions.forEach(function(element, index){
+													if(item.champion == element.id && element.id != 0){
+														exist = false;
+														matchList.forEach(function(match, index){
+															if(element.id == match.id){
+																exist = true;
+															}
+														});
+														if(!exist){
+															matchList.push(element);
+														}
+													}
+												});
+											});
+										matchList.sort(function(a,b) {
+										    return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
+										});
 
-						res.json(matchList.slice(-3));
+										res.json(matchList.slice(-3));
+									}
+							});
 					}
-				});
-			}
 		});
 	}
 
