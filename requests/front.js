73cd8f148c847003c4090c3e1826fea1352c14e7
Pedro Soares <pedrosoares1542@gmail.com>
2016-05-01 11:52:05 -0300

    adicionando buscar principais champs por lane

--- requests/front.js
+++ requests/front.js
@@ -29,27 +29,53 @@ module.exports = {
 		});
 	},
 
-	getChampionStatsBySummonerId = function(req, res){
+	getChampionStatsBySummonerId(req, res){
 		let id = req.body.id;
 		let region = req.body.region;
 		let lane = req.body.lane;
+		let matchListLane = {}, exist;
+
+		request('https://br.api.pvp.net/api/lol/'+ region +'/v2.2/matchlist/by-summoner/'+ id +'?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016&api_key='+ key, function(error, response, body){
+			if(!error && response.statusCode == 200) {
+				let match = JSON.parse(body), matchListLane = {};
+				matchList.matches.forEach(function(item, index){
+					if(item.lane == lane || item.role == lane){
+						exist = false;
+						matchListLane.forEach(function(element, index){
+							if(item.champion == element.champion){
+								exist = true;
+							}
+						});
+						if(!exist){
+							matchListLane.push(item);
+						}
+					}
+				});
+			}
+			console.log(matchListLane);
+			res.json(matchListLane);
+
+		});
 
 		request('https://br.api.pvp.net/api/lol/'+ region +'/v1.3/stats/by-summoner/'+ id + '/ranked?api_key='+ key, function (error, response, body) {
 			if(!error && response.statusCode == 200) {
-				let stats = JSON.parse(body);			
-				stats.champions.forEach(function(item, index){
-					if(item.id == 0){
-						stats.champions.splice(index,1);
-					}
+				let stats = JSON.parse(body), matchList = {};
+					matchListLane.forEach(function(item, index){
+						stats.champions.forEach(function(element, index){
+							if(item.champion == element.id && element.id != 0){
+								matchList.push(element);
+							}
+						});
+					});
+
+				matchList.sort(function(a,b) {
+				    return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
 				});
 
-				stats.champions.sort(function(a,b) {
-			    	return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
-				});
-
-				res.json(stats.champions.slice(-3));
+				res.json(matchList.slice(-3));
 			}
 		});
 	}
+
 
 }
