ca9b44ef81983925b04556b0525ed480194aaa95
Pedro Soares <pedrosoares1542@gmail.com>
2016-05-01 13:12:28 -0300

    finalizando buscar champions pela lane

--- requests/front.js
+++ requests/front.js
@@ -32,47 +32,42 @@ module.exports = {
 	getChampionStatsBySummonerId(req, res){
 		let id = req.body.id;
 		let region = req.body.region;
-		let lane = req.body.lane;
-		let matchListLane = {}, exist;
+		let lane = req.body.lane; //LANES: DUO_SUPPORT, DUO_CARRY, MID ,JUNGLE, TOP
+		let matchListLane = [],exist;
 
 		request('https://br.api.pvp.net/api/lol/'+ region +'/v2.2/matchlist/by-summoner/'+ id +'?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016&api_key='+ key, function(error, response, body){
 			if(!error && response.statusCode == 200) {
-				let match = JSON.parse(body), matchListLane = {};
-				matchList.matches.forEach(function(item, index){
+				let match = JSON.parse(body);
+				match.matches.forEach(function(item, index){
 					if(item.lane == lane || item.role == lane){
-						exist = false;
-						matchListLane.forEach(function(element, index){
-							if(item.champion == element.champion){
-								exist = true;
-							}
-						});
-						if(!exist){
-							matchListLane.push(item);
-						}
+						matchListLane.push(item);
 					}
 				});
-			}
-			console.log(matchListLane);
-			res.json(matchListLane);
-
-		});
-
-		request('https://br.api.pvp.net/api/lol/'+ region +'/v1.3/stats/by-summoner/'+ id + '/ranked?api_key='+ key, function (error, response, body) {
-			if(!error && response.statusCode == 200) {
-				let stats = JSON.parse(body), matchList = {};
-					matchListLane.forEach(function(item, index){
-						stats.champions.forEach(function(element, index){
-							if(item.champion == element.id && element.id != 0){
-								matchList.push(element);
-							}
+				request('https://br.api.pvp.net/api/lol/'+ region +'/v1.3/stats/by-summoner/'+ id + '/ranked?api_key='+ key, function (error, response, body) {
+					if(!error && response.statusCode == 200) {
+						let stats = JSON.parse(body), matchList = [];
+							matchListLane.forEach(function(item, index){
+								stats.champions.forEach(function(element, index){
+									if(item.champion == element.id && element.id != 0){
+										exist = false;
+										matchList.forEach(function(match, index){
+											if(element.id == match.id){
+												exist = true;
+											}
+										});
+										if(!exist){
+											matchList.push(element);
+										}
+									}
+								});
+							});
+						matchList.sort(function(a,b) {
+						    return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
 						});
-					});
 
-				matchList.sort(function(a,b) {
-				    return a.stats.totalSessionsPlayed < b.stats.totalSessionsPlayed ? -1 : a.stats.totalSessionsPlayed > b.stats.totalSessionsPlayed ? 1 : 0;
+						res.json(matchList.slice(-3));
+					}
 				});
-
-				res.json(matchList.slice(-3));
 			}
 		});
 	}
